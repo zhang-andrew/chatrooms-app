a28db48eb64828b2ec6fd765c0800c2626a4b4f5
Andrew Zhang <azhang@192-168-1-180.tpgi.com.au>
2022-10-28 03:49:57 +1000

    environt.prod done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyA_kz1YmttZUfYD_veq9ykQ-_6HEcNqPRc",
+        authDomain: "mario-a9c5f0.firebaseapp.com",
+        projectId: "mario-a9c5f0",
+        storageBucket: "mario-a9c5f0.appspot.com",
+        messagingSenderId: "225487082125",
+        appId: "1:225487082125:web:9295158f81ed348bc088c4",
+        measurementId: "G-TJ4Z91NLGB"
+    }
 };
