46de50a5bf9973d8439a62eb0568f94bb068818c
Andrew Zhang <azhang@192-168-1-180.tpgi.com.au>
2022-11-02 19:22:41 +1000

    changed firebaseconfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,23 @@
 
 export const environment = {
     production: false,
+    // firebaseConfig: {
+    //     apiKey: "AIzaSyA_kz1YmttZUfYD_veq9ykQ-_6HEcNqPRc",
+    //     authDomain: "mario-a9c5f0.firebaseapp.com",
+    //     projectId: "mario-a9c5f0",
+    //     storageBucket: "mario-a9c5f0.appspot.com",
+    //     messagingSenderId: "225487082125",
+    //     appId: "1:225487082125:web:9295158f81ed348bc088c4",
+    //     measurementId: "G-TJ4Z91NLGB"
+    // },
     firebaseConfig: {
-        apiKey: "AIzaSyA_kz1YmttZUfYD_veq9ykQ-_6HEcNqPRc",
-        authDomain: "mario-a9c5f0.firebaseapp.com",
-        projectId: "mario-a9c5f0",
-        storageBucket: "mario-a9c5f0.appspot.com",
-        messagingSenderId: "225487082125",
-        appId: "1:225487082125:web:9295158f81ed348bc088c4",
-        measurementId: "G-TJ4Z91NLGB"
+        apiKey: "AIzaSyA80M-BHjYtxi5y8twTkNPuNtn2jBe1D6g",
+        authDomain: "chatrooms-web-app.firebaseapp.com",
+        projectId: "chatrooms-web-app",
+        storageBucket: "chatrooms-web-app.appspot.com",
+        messagingSenderId: "120449081568",
+        appId: "1:120449081568:web:1e906a758b224e7059f085",
+        measurementId: "G-LPZLDDQGQC"
     }
 };
 
